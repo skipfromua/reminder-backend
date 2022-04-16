aa506d751d92d5ef0eb4d7412021879cd90d2f60
Oleksandr Sherberh <skipfromua@gmail.com>
2022-04-16 21:28:21 +0300

    added user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get 'home/index'
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
