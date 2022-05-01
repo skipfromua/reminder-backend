b1b6085e4effe28ec1898849f34174053715d306
Oleksandr Sherberh <skipfromua@gmail.com>
2022-05-01 21:25:52 +0300

    added react navbar component

--- app/javascript/Components/navigationPanel/navigationPanel.jsx
+++ app/javascript/Components/navigationPanel/navigationPanel.jsx
@@ -1,6 +1,18 @@
 import React from 'react';
+import NavbarAuth from './NavbarAuth/NavbarAuth';
+import NavbarIconContainer from './NavbarIconContainer/NavbarIconContainer';
+import NavbarItems from './NavbarItems/NavbarItems';
 
+import classes from './NavigationPanel.module.css';
 
-export default navigationPanel = () => {
-  
+const NavigationPanel = () => {
+  return (
+    <nav className={classes.navbarBackground}>
+      <NavbarIconContainer />
+      <NavbarItems />
+      <NavbarAuth />
+    </nav>
+  )
 }
+
+export default NavigationPanel;
