cb96519d17b4732e61d53e8ce324c23dab8c227e
Oleksandr Sherberh <skipfromua@gmail.com>
2023-02-19 11:56:49 +0200

    Fix serializers

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -2,5 +2,9 @@ class EventSerializer
   include FastJsonapi::ObjectSerializer
   set_id :id
 
-  attributes :id, :name, :date
+  attributes :id, :name
+
+  attribute :date do |event|
+    event.date&.strftime("%m-%d")
+  end
 end
--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -2,5 +2,9 @@ class NotificationSerializer
   include FastJsonapi::ObjectSerializer
   set_id :id
 
-  attributes :id, :enabled, :start_notifying_days_before, :notify_at
+  attributes :id, :enabled, :start_notifying_days_before
+
+  attribute :notify_at do |notification|
+    notification.notify_at&.strftime("%H:%M")
+  end
 end
